fca56015c7406fe1c6f20458f1ec0728db830e98
Seth Ullman <seth.ullman98@gmail.com>
2020-07-10 11:59:28 -0600

    add unoptimized solution

---
+++ Leetcode JS/Longest Pallindrome.js
@@ -0,0 +1,25 @@
+var longestPalindrome = function (s) {
+  if (s.length == 1) return s;
+
+  let subs = [];
+
+  for (let i = 0; i < s.length + 1; i++) {
+    for (let j = i + 1; j < s.length + 1; j++) {
+      if (!subs.includes(s.slice(i, j))) subs.push(s.slice(i, j));
+    }
+  }
+
+  let longest = "";
+
+  for (let sub = 0; sub < subs.length; sub++) {
+    let current = subs[sub];
+    if (current.length > longest.length) {
+      let reversed = current.split("").reverse().join("");
+      if (reversed == current) {
+        longest = current;
+      }
+    }
+  }
+
+  return longest;
+};
