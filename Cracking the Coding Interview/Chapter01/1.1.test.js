9fc5e61da4d3538e946b441205ec91b53ff99356
Seth Ullman <seth.ullman98@gmail.com>
2020-07-20 11:02:00 -0600

    add more tests for 1.1

--- Cracking the Coding Interview/Chapter01/1.1.test.js
+++ Cracking the Coding Interview/Chapter01/1.1.test.js
@@ -4,3 +4,10 @@ test("expect 'abcdefghijklmnopqrstuvwxyz' to return true", () => {
   expect(isUnique('abcdefghijklmnopqrstuvwxyz')).toBe(true);
 });
 
+test("expect 'abc' to return true", () => {
+  expect(isUnique('abc')).toBe(true);
+});
+
+test("expect 'aabbcc' to return false", () => {
+  expect(isUnique('aabbcc')).toBe(false);
+});
