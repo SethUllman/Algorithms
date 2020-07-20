19a3828816791fbdf15db1cd3139de6988cf634c
Seth Ullman <seth.ullman98@gmail.com>
2020-07-20 11:13:00 -0600

    add test cases for 1.2

--- Cracking the Coding Interview/Chapter01/1.2.test.js
+++ Cracking the Coding Interview/Chapter01/1.2.test.js
@@ -1,1 +1,18 @@
 const checkPermutation = require('./1.2');
+
+
+test("expect 'ABC', 'BCA' to return true", () => {
+  expect(checkPermutation('ABC', 'BCA')).toBe(true)
+});
+
+test("expect 'ABC', 'DEF' to return false", () => {
+  expect(checkPermutation('ABC', 'DEF')).toBe(false)
+});
+
+test("expect 'ABC', 'BCD' to return false", () => {
+  expect(checkPermutation('ABC', 'BCD')).toBe(false)
+});
+
+test("expect 'ABC', 'abc' to return false", () => {
+  expect(checkPermutation('ABC', 'abc')).toBe(false)
+});
