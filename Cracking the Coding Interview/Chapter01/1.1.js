5302561caf9126a600d38f835859e578303c6754
Seth Ullman <seth.ullman98@gmail.com>
2020-07-20 11:00:24 -0600

    add completed isUnique function

--- Cracking the Coding Interview/Chapter01/1.1.js
+++ Cracking the Coding Interview/Chapter01/1.1.js
@@ -1,8 +1,19 @@
 //Implement an algorithm to determine if a string has all unique characters. 
 //What if you cannot use additional data structures?
 
-function isUnique() {
+function isUnique(string) {
+  let charHash = {};
 
+  for(let i = 0; i < string.length; i++){
+    const char = string[i];
+    if (charHash[char] == undefined){
+      charHash[char] = char;
+    } else {
+      return false;
+    }
+  }
+
+  return true;
 }
 
 module.exports = isUnique;
--- Cracking the Coding Interview/Chapter01/1.1.test.js
+++ Cracking the Coding Interview/Chapter01/1.1.test.js
@@ -3,3 +3,4 @@ const isUnique = require('./1.1');
 test("expect 'abcdefghijklmnopqrstuvwxyz' to return true", () => {
   expect(isUnique('abcdefghijklmnopqrstuvwxyz')).toBe(true);
 });
+
