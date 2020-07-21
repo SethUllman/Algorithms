af5d093b2ca3d00dd51ece160a3fb4f1ca401ce4
Seth Ullman <seth.ullman98@gmail.com>
2020-07-21 10:31:19 -0600

    add completed function

--- Cracking the Coding Interview/Chapter01/1.3.js
+++ Cracking the Coding Interview/Chapter01/1.3.js
@@ -1,13 +1,24 @@
 // Write a method to replace all spaces in a string with '%20'. 
 //You may assume that the string has sufficient space at the end to hold 
-//theadditional characters, and that you are given the "true" length of the string.
+//the additional characters, and that you are given the "true" length of the string.
 
 //EXAMPLE
 //Input: 'Mr John Smith      ', 13
 //Output: 'Mr%20John%20Smith'
 
 function URLify(string, length) {
+  let newString = [];
 
+  for(let i = 0; i < length; i++){
+    let current = string[i];
+    if (current == ' '){
+      newString.push('%20');
+    } else {
+      newString.push(current);
+    }
+  }
+
+  return newString.join('');
 }
 
 module.exports = URLify;
