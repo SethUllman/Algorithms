12595f1a2566ed3b17286fc51388792dc59a5b14
Seth Ullman <seth.ullman98@gmail.com>
2020-07-21 10:43:21 -0600

    add 1.4 test cases

--- Cracking the Coding Interview/Chapter01/1.4.test.js
+++ Cracking the Coding Interview/Chapter01/1.4.test.js
@@ -1,1 +1,13 @@
 const PalindromePermutation = require('./1.4');
+
+test('expect "Tact Coa" to return true', () => {
+  expect(PalindromePermutation("Tact Coa")).toBe(true)
+});
+
+test('expect "abcde" to return false', () => {
+  expect(PalindromePermutation("abcde")).toBe(false)
+});
+
+test('expect "a" to return true', () => {
+  expect(PalindromePermutation("a")).toBe(true)
+});
