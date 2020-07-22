1b266896aeaf519737d79362ec3261958e26ca7b
Seth Ullman <seth.ullman98@gmail.com>
2020-07-22 11:48:24 -0600

    add completed algo

--- Cracking the Coding Interview/Chapter01/1.4.js
+++ Cracking the Coding Interview/Chapter01/1.4.js
@@ -8,6 +8,27 @@
 //Output:  True (permutations: "taco cat", "atco cta", etc)
 
 function PalindromePermutation(string){
+
+  let alph = 'abcdefghijklmnopqrstuvwxyz';
+
+  let newString = [];
+
+  for(let j = 0; j < string.length; j++){
+    let current = string[j];
+    if(alph.includes(current.toLowerCase())) newString.push(current.toLowerCase());
+  }
+  
+  let permutationsArray = Permutations(newString.join(''));
+
+  for(let i = 0; i < permutationsArray.length; i++){
+    let perm = permutationsArray[i];
+    if(perm == perm.split('').reverse().join('')) return true;
+  }
+
+  return false;
+}
+
+function Permutations(string) {
   if (!string || typeof string !== "string") {
     return "Please enter a string"
   } else if (string.length < 2) {
@@ -20,15 +41,15 @@ function PalindromePermutation(string){
     let char = string[i]
 
     if (string.indexOf(char) != i)
-    continue;
+      continue;
 
     let remainingChars = string.slice(0, i) + string.slice(i + 1, string.length)
 
-    for (let permutation of PalindromePermutation(remainingChars)) {
+    for (let permutation of Permutations(remainingChars)) {
       permutationsArray.push(char + permutation)
     }
   }
-  return permutationsArray
+  return permutationsArray;
 }
 
 module.exports = PalindromePermutation;
