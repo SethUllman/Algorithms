a31b75625668d8dd2d4ecf4e6f4a0b5bf2b9f161
Seth Ullman <seth.ullman98@gmail.com>
2020-07-22 11:37:19 -0600

    add permutation solution

--- Cracking the Coding Interview/Chapter01/1.4.js
+++ Cracking the Coding Interview/Chapter01/1.4.js
@@ -8,7 +8,27 @@
 //Output:  True (permutations: "taco cat", "atco cta", etc)
 
 function PalindromePermutation(string){
-  
+  if (!string || typeof string !== "string") {
+    return "Please enter a string"
+  } else if (string.length < 2) {
+    return string
+  }
+
+  let permutationsArray = []
+
+  for (let i = 0; i < string.length; i++) {
+    let char = string[i]
+
+    if (string.indexOf(char) != i)
+    continue;
+
+    let remainingChars = string.slice(0, i) + string.slice(i + 1, string.length)
+
+    for (let permutation of PalindromePermutation(remainingChars)) {
+      permutationsArray.push(char + permutation)
+    }
+  }
+  return permutationsArray
 }
 
 module.exports = PalindromePermutation;
--- Cracking the Coding Interview/Chapter01/1.4.test.js
+++ Cracking the Coding Interview/Chapter01/1.4.test.js
@@ -4,14 +4,14 @@ test('expect "Tact Coa" to return true', () => {
   expect(PalindromePermutation("Tact Coa")).toBe(true)
 });
 
-test('expect "abcde" to return false', () => {
-  expect(PalindromePermutation("abcde")).toBe(false)
+test('expect "abc" to return false', () => {
+  expect(PalindromePermutation("abc")).toBe(false)
 });
 
 test('expect "a" to return true', () => {
   expect(PalindromePermutation("a")).toBe(true)
 });
 
-test('expect "$Tact *&Coa" to return true', () => {
-  expect(PalindromePermutation("$Tact *&Coa")).toBe(true)
+test('expect "$a &a" to return true', () => {
+  expect(PalindromePermutation("$a &a")).toBe(true)
 });
