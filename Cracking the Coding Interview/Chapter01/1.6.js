8753917ee6c40a252c2839b54aead0ce864d693a
Seth Ullman <seth.ullman98@gmail.com>
2020-07-28 15:13:38 -0600

    add 1.6 boilerplate

---
+++ Cracking the Coding Interview/Chapter01/1.6.js
@@ -0,0 +1,10 @@
+//Implement a method to perform basic string compression using the counts of repeated characters. 
+//For example, the string 'aabcccccaaa woul become a1b1c5a3. If the complressed 
+//string would not become smaller than the original string, your method should return 
+//the original string. You can assume the string has only uppercase and lowercase letters(a-z).
+
+const stringCompression = (string) => {
+
+}
+
+module.exports = stringCompression;
---
+++ Cracking the Coding Interview/Chapter01/1.6.test.js
@@ -0,0 +1,1 @@
+const stringCompression = require('./1.6');
