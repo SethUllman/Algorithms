25ee506d6f7967d8b118003cacacdd0e39f46c5a
Seth Ullman <seth.ullman98@gmail.com>
2020-07-28 15:24:06 -0600

    add test cases

--- Cracking the Coding Interview/Chapter01/1.6.js
+++ Cracking the Coding Interview/Chapter01/1.6.js
@@ -1,10 +1,10 @@
 //Implement a method to perform basic string compression using the counts of repeated characters. 
-//For example, the string 'aabcccccaaa woul become a1b1c5a3. If the complressed 
+//For example, the string 'aabcccccaaa would become a2b1c5a3. If the compressed 
 //string would not become smaller than the original string, your method should return 
 //the original string. You can assume the string has only uppercase and lowercase letters(a-z).
 
 const stringCompression = (string) => {
-
+  
 }
 
 module.exports = stringCompression;
--- Cracking the Coding Interview/Chapter01/1.6.test.js
+++ Cracking the Coding Interview/Chapter01/1.6.test.js
@@ -1,1 +1,14 @@
 const stringCompression = require('./1.6');
+
+test('expect stringCompression("abc") to return "abc".', () => {
+  expect(stringCompression('abc')).toMatch(/abc/);
+});
+
+test('expect stringCompression("abbcccdddd") to return "a1b2c3d4"', () => {
+  expect(stringCompression('abbcccdddd')).toMatch(/a1b2c3d4/);
+});
+
+test('expect stringCompression("aabbcc") to return "aabbcc"', () => {
+  expect(stringCompression('aabbcc')).toMatch(/aabbcc/);
+});
+
