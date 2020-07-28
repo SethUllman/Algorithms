01950fec3d14fb7df53904144db6db875001a8db
Seth Ullman <seth.ullman98@gmail.com>
2020-07-28 17:09:02 -0600

    add finished function

--- Cracking the Coding Interview/Chapter01/1.6.js
+++ Cracking the Coding Interview/Chapter01/1.6.js
@@ -4,7 +4,31 @@
 //the original string. You can assume the string has only uppercase and lowercase letters(a-z).
 
 const stringCompression = (string) => {
+  let compressed = '';
+  let currentChar = '';
+  let count = 0;
   
+
+  for (let i = 0; i < string.length; i++){
+    if (count == 0){
+      currentChar = string[i]; 
+      compressed += currentChar; 
+      count++; 
+    } else if(count > 0 && string[i] == currentChar){
+      count++;
+    } else if(count > 0 && string[i] != currentChar){
+      compressed += count; 
+      currentChar = string[i]; 
+      compressed += currentChar;
+      count = 1;
+    }
+
+  }
+  if(count > 1){
+    compressed += count;
+  }
+  return (string.length <= compressed.length ? string : compressed);
+
 }
 
 module.exports = stringCompression;
