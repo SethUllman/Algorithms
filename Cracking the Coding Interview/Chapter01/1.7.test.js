c86628eff267fb5bb082002af8b77b16920092ce
Seth Ullman <seth.ullman98@gmail.com>
2020-07-30 10:34:48 -0600

    add test cases

--- Cracking the Coding Interview/Chapter01/1.7.test.js
+++ Cracking the Coding Interview/Chapter01/1.7.test.js
@@ -1,1 +1,17 @@
 const rotateMatrix = require('./1.7.js');
+
+const image = [[1,2,3],
+               [4,5,6],
+               [7,8,9]];
+
+const result = [[7,4,1],
+                [8,5,2],
+                [9,6,3]];
+
+test('expect image to rotate clockwise 90 degrees', () => {
+  expect(rotateMatrix(image)).toEqual(result);
+});
+
+test('expect rotateMatrix to rotate matrix', () => {
+  expect(rotateMatrix(image)).not.toEqual(image);
+});
