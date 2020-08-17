1de036264a90fd91333da42972620395fccc7e26
Seth Ullman <seth.ullman98@gmail.com>
2020-08-17 10:34:13 -0600

    add boilerplate

---
+++ Cracking the Coding Interview/Chapter02/2.1.js
@@ -0,0 +1,10 @@
+//Write code to remove duplicates from an unsorted linked list
+
+// input: 5 => 2 => 1 => 2 => 3 => 7 => 3 => 1
+// output: 5 => 2 => 1 => 3 => 7
+
+const removeDups = (linkedList) => {
+
+}
+
+module.exports = removeDups;
