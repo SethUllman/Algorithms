ac64f206acc432f20680cfdf3a02948a0c3d9ca3
Seth Ullman <seth.ullman98@gmail.com>
2020-08-20 13:22:32 -0600

    add finished function

--- Cracking the Coding Interview/Chapter02/2.2.js
+++ Cracking the Coding Interview/Chapter02/2.2.js
@@ -1,7 +1,16 @@
 //Implement an algorithm to find the kth to last element of a singly linked list.
 
 const returnKthLast = (linkedList, k) => {
+  let nodes = [linkedList];
 
+  let nextNode = linkedList.next;
+
+  while(nextNode != null){
+    nodes.push(nextNode);
+    nextNode = nextNode.next;
+  }
+
+  return nodes[nodes.length-k];
 }
 
 module.exports = returnKthLast;
