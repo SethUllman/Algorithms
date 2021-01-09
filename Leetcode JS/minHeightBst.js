43b98ffc10cdeba1aaedfd869d253981a835a884
Seth Ullman <seth.ullman98@gmail.com>
2021-01-08 23:45:54 -0700

    add minHeightBst problem

---
+++ Leetcode JS/minHeightBst.js
@@ -0,0 +1,12 @@
+// Write a function that takes in a non-empty sorted array of distinct integers,
+// constructs a BST from the integers, and returns the root of the BST.BST
+// The function should minimize the height of the BST.BST
+// You've been provided with a BST class that you'll have to use to construct the BST.
+
+// Each BST node has an integer IDBCursorWithValue, a left child node, and a right 
+// chile node. A node is said to be a valid BST node if and only if it satisfies the BST property:
+// its value is strictly greater than the values of every node to its left; its value is less than or equal to the values of every node to its right;
+// and its children nodes are either valid BST nodes themselves or None / null.BST
+// A bst is valid if and only if all of its nodes are valid BST nodes.BST
+// Note that the bst class already has an insert method which you can use if you WebAuthentication.
+
